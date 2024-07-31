bd41f16b788e2b31e32eebcb2466049572555e3f
Ayan Sheikh <ayansheikh52@gmail.com>
2024-07-31 17:29:12 +0530

    fixed margin

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default async function Home() {
                     Get Started
                   </Link>
                   <Link
-                    className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                    className="inline-flex h-9 mt-3 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
                     href="/create/ai"
                   >
                     <span className="mr-2">Get Started with AI</span> <Sparkles className="size-5" />

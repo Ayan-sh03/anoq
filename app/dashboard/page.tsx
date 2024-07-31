18f780e9bb1609abd9faff068b75c44b141103dc
Ayan Sheikh <ayansheikh52@gmail.com>
2024-07-31 19:28:58 +0530

    fixed margin

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import FormCard from "@/components/FormCard";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Form } from "@/dbschema/interfaces";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Link from "next/link";
@@ -38,7 +37,7 @@ function Dashboard({ data }: { data: Form[] }) {
     <div className="flex flex-col w-full min-h-screen container">
       <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] bg-gray-100/40 flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 dark:bg-gray-800/40">
         <div className="max-w-6xl w-full mx-auto flex items-center gap-4">
-          <form className="flex-1">
+          {/* <form className="flex-1">
             <Input
               placeholder="Search forms..."
               className="bg-white dark:bg-gray-950"
@@ -46,7 +45,7 @@ function Dashboard({ data }: { data: Form[] }) {
             <Button type="submit" className="sr-only">
               Submit
             </Button>
-          </form>
+          </form> */}
           <Link href="/create"><Button>Create New</Button></Link>
         </div>
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 max-w-6xl w-full mx-auto" >
